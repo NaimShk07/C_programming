7de9004ba7a84744a7cb8d557161e473767d06f0
Naim619 <nayemuddinshaikh@gmail.com>
2022-11-16 20:09:02 +0530

    v1.7

---
+++ Assingment/Module_3/que01.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+       printf("Nayemuddin Shaikh \n");
+       printf("5/7/2004 \n");
+       printf("18 \n");
+       printf("Danilimda, Ahmedabad \n");
+return 0;
+}
---
+++ Assingment/Module_3/que09.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+       int a=10,b=20;
+       a=b;
+       // b=10;
+       b=a-10;
+      
+
+       printf("%d %d",a , b);
+
+      
+return 0;
+}
