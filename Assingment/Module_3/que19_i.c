ef0f43e3bb228b7ba41413ae3da29082227d5fea
Naim619 <nayemuddinshaikh@gmail.com>
2022-11-18 15:47:29 +0530

    updated

--- Assingment/Module_3/que19_i.c
+++ Assingment/Module_3/que19_i.c
@@ -1,9 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
 
-       //write a program to make sum of given numbers
+       // write a program to make sum of given numbers
+       int num[4];
+       int total=0;
+       for (int i = 0; i < 4; i++)
+       {
+       printf("Enter %d numbers:",i+1);
+              scanf("%d", &num[i]);
+       }
+       for (int i = 0; i < 4; i++)
+       {
+             total=num[i]+total;
+       }
+       printf("Total of four numbers is %d",total);
+       
 
-       printf("Enter 4 digit number");
-return 0;
+       return 0;
 }
